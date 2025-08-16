786f8a2eea50b3546c3627277f3e44abe2b379b2
are305 <are305@ensign.edu>
2025-08-15 18:43:10 -0600

    Attempting another method to solve image loading.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-// Detect repo name from environment (for GitHub Pages)
+// Read repo name for GitHub Pages
 const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split("/")[1] : "";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+    // Important for GitHub Pages - no /_next/image
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
